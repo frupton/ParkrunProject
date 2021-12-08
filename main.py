98d9c066c4ef9e307d342a4267a14f705185fed9
frupton <73693295+frupton@users.noreply.github.com>
2021-12-08 11:35:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import webbrowser
 import os
 import pandas as pd
 import sounds
-import numpy as np
+#import visual
 
 
 def get_home(file):
-    home = []
+    home_coord = []
     raw_data = pd.read_csv(file)
-    home += list(raw_data['Lat'])
-    home += list(raw_data['Long'])
-    return home
+    home_coord += list(raw_data['Lat'])
+    home_coord += list(raw_data['Long'])
+    return home_coord
 
 
 def get_full_list():
-
     place, lat, long, junior = ([] for i in range(4))
     raw_data = pd.read_csv('Parkrun-locations.csv')
 
@@ -28,11 +27,43 @@ def get_full_list():
 
 
 def get_run_list(file):
-
     done = []
     raw_data = pd.read_csv(file)
     done += list(raw_data['Place'])
     return done
+
+
+def done_labels(parkrun_places, parkrun_run):
+    done = []
+    for i in range(len(parkrun_places)):
+        if parkrun_places[i] in parkrun_run:
+            done.append(1)
+        else:
+            done.append(0)
+    return done
+
+
+def show_label(show, bin_label, label, type_label):
+    if show:
+        for i in range(len(place)):
+            if bin_label[i]:
+                type_label[i] = label
+    return type_label
+
+
+def alphabet_labels(parkrun_places, parkrun_run):
+    alphabet_done = []
+    for i in range(len(parkrun_run)):
+        letter = parkrun_run[i][0]
+        if letter not in alphabet_done:
+            alphabet_done.append(letter)
+    for i in range(len(parkrun_places)):
+        letter = parkrun_places[i][0]
+        if letter not in alphabet_done:
+            alphabet_done.append(1)
+        else:
+            alphabet_done.append(0)
+    return alphabet_done
 
 
 def folium_func(name, lats, longs, types_list, home):
@@ -49,8 +80,8 @@ def folium_func(name, lats, longs, types_list, home):
         """
 
     # dictionary with keys = point type, values = icon
-    icons_dict = {'done': 'check', 'not_done': 'star', 'junior': 'child'}
-    colour_dict = {'done': 'green', 'not_done': 'lightgray', 'junior': 'beige'}
+    icons_dict = {'done': 'check', 'not_done': 'star', 'junior': 'child', 'alphabet': 'star'}
+    colour_dict = {'done': 'green', 'not_done': 'lightgray', 'junior': 'beige', 'alphabet': 'orange'}
 
     # Folium Stuff - Make an empty map
     m = folium.Map(location=home, zoom_start=10, tiles='OpenStreetMap')
@@ -61,7 +92,7 @@ def folium_func(name, lats, longs, types_list, home):
 
     # Add marker one by one on the map
     for j in range(1, len(name)):
-        if types[j] != 'no':
+        if types_list[j] != 'no':
             folium.Marker(
                 location=[lats[j], longs[j]],
                 popup=name[j], icon=folium.Icon(color=colour_dict[types_list[j]], icon=icons_dict[types_list[j]],
@@ -74,58 +105,33 @@ def folium_func(name, lats, longs, types_list, home):
 
 
 if __name__ == '__main__':
+    # This section will be replaced with info from the GUI
     file_name_home = 'home_locations.csv'
     file_name_done = 'Parkrun-done.csv'
-    juniors = 0
-    alphabet_challenge = 1
+    show_juniors = 0
+    show_alphabet = 0
+    show_done = 0
+    show_not_done = 0
 
-    place, lat, long, junior = get_full_list()
+    # Set up
+    place, lat, long, junior_bin = get_full_list()
     home = get_home(file_name_home)
     run = get_run_list(file_name_done)
-    types = []
-    alphabet = []
 
-    # Label the locations for if they are jun
-    for i in range(len(place)):
-        if place[i] in run:
-            types.append('done')
-        else:
-            if junior[i] == 1:
-                types.append('junior')
-            else:
-                types.append('not_done')
+    # 1s and 0s
+    done_bin = done_labels(place, run)
+    alphabet_bin = alphabet_labels(place, run)
 
-    # Alphabet Challenge labels
-    alphabet_done = []
-    for i in range(len(run)):
-        letter = run[i][0]
-        if letter not in alphabet_done:
-            alphabet_done.append(letter)
-    for i in range(len(place)):
-        letter = place[i][0]
-        if letter not in alphabet_done:
-            alphabet.append(1)
-        else:
-            alphabet.append(0)
-
-    # Culling
-    if juniors == 0:
-        for i in range(len(types)):
-            if types[i] == 'junior':
-                types[i] = 'no'
-
-    if alphabet_challenge == 1:
-        for i in range(len(place)):
-            if alphabet[i] == 0:
-                types[i] = 'no'
+    type_labels = ['no']*len(place)
+    type_labels = show_label(show_juniors, junior_bin, 'junior', type_labels)
+    type_labels = show_label(show_alphabet, alphabet_bin, 'alphabet', type_labels)
+    type_labels = show_label(show_done, done_bin, 'done', type_labels)
 
     # Calling folium_func
-    x = folium_func(place, lat, long, types, home)
+    x = folium_func(place, lat, long, type_labels, home)
     print(x)
 
+    filename = 'file:///' + os.getcwd() + '/' + 'map.html'
+    webbrowser.open_new_tab(filename)
 
-filename = 'file:///' + os.getcwd() + '/' + 'map.html'
-webbrowser.open_new_tab(filename)
-
-
-sounds.buzz()
+    sounds.buzz()
